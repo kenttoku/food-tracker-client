d0d37df1cdc9d6ad92e2243aaab3946708b32b88
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-22 16:48:58 -0400

    Style navbar for desktop

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function HeaderBar(props) {
   return (
-    <header>
-      <h2 className="screen-header">{props.title}</h2>
-    </header>
+    <div>
+      <header>
+        <h2 className="screen-header">{props.title}</h2>
+      </header>
+    </div>
   );
 }
