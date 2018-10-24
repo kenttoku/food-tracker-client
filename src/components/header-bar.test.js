8254d177ba6e862d74edb7bbdba3351e1d25f332
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-24 14:00:18 -0400

    Finish header-bar tests

--- src/components/header-bar.test.js
+++ src/components/header-bar.test.js
@@ -7,4 +7,12 @@ describe('<HeaderBar />', () => {
   it('Renders without crashing', () => {
     shallow(<HeaderBar />);
   });
+
+
+  it('Displays the correct header', () => {
+    const title='title';
+    const wrapper = shallow(<HeaderBar title={title} />);
+    const header = wrapper.find('.screen-header');
+    expect(header.first().text()).toBe(title);
+  });
 });
