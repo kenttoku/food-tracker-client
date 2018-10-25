0ee3ceb04110dc22ccc35948773864834e076573
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-25 16:45:27 -0400

    Add layout to landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -2,9 +2,9 @@ import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { Link, Redirect } from 'react-router-dom';
-// Assets
 import './buttons.css';
 import './landing-page.css';
+import img from '../assets/baseline-add_circle-24px.svg';
 
 export function LandingPage(props) {
   if (props.loggedIn) {
@@ -28,13 +28,32 @@ export function LandingPage(props) {
           </Link>
         </div>
       </section>
-      <section>
-
-        <p>
+      <section className="feature left">
+        <img src={img}/>
+        <div className="desc">
+          <h4>Lorem ipsum</h4>
+          <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique arcu sed justo pharetra, in dictum quam dapibus. Integer rutrum dui in purus ultrices, at gravida felis suscipit. Aenean nisl odio, dapibus ac felis vitae, molestie placerat ligula. Maecenas quis purus quam. Nullam porttitor in arcu sed tincidunt. Suspendisse nec accumsan orci. Vivamus ligula ex, tincidunt sed pulvinar vel, congue vitae ipsum.
-
-
-        </p>
+          </p>
+        </div>
+      </section>
+      <section className="feature right">
+        <img src={img} />
+        <div className="desc">
+          <h4>Lorem ipsum</h4>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique arcu sed justo pharetra, in dictum quam dapibus. Integer rutrum dui in purus ultrices, at gravida felis suscipit. Aenean nisl odio, dapibus ac felis vitae, molestie placerat ligula. Maecenas quis purus quam. Nullam porttitor in arcu sed tincidunt. Suspendisse nec accumsan orci. Vivamus ligula ex, tincidunt sed pulvinar vel, congue vitae ipsum.
+          </p>
+        </div>
+      </section>
+      <section className="feature left">
+        <img src={img}/>
+        <div className="desc">
+          <h4>Lorem ipsum</h4>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tristique arcu sed justo pharetra, in dictum quam dapibus. Integer rutrum dui in purus ultrices, at gravida felis suscipit. Aenean nisl odio, dapibus ac felis vitae, molestie placerat ligula. Maecenas quis purus quam. Nullam porttitor in arcu sed tincidunt. Suspendisse nec accumsan orci. Vivamus ligula ex, tincidunt sed pulvinar vel, congue vitae ipsum.
+          </p>
+        </div>
       </section>
     </div>
 
