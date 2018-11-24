56d60c0ea90f5fc9d4668fc6b8f0d25d1b0627da
Kent Tokunaga <kenttokunaga@gmail.com>
2018-11-24 03:50:46 -0500

    Add nprogress

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,19 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+
 import Navbar from './Navbar';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Header = () => (
   <div>
